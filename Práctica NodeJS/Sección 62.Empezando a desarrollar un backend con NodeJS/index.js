b30af0a5b33f7db2edd12b1af84ae2569a86ad8e
Diego Nicolas Collazo <diego.nicolas@outlook.com.ar>
2020-05-25 01:47:47 -0300

    JS.Seccion 62

---
+++ Práctica NodeJS/Sección 62.Empezando a desarrollar un backend con NodeJS/index.js
@@ -0,0 +1,21 @@
+"use strict";
+var mongoose = require("mongoose");
+var app = require("./app.js");
+var port = 3700;
+
+mongoose.Promise = global.Promise;
+mongoose
+	.connect("mongodb://localhost:27017/portafolio", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("Conexión a la base de datos establecida.");
+		// Creacion del servidor
+		app.listen(port, () => {
+			console.log(
+				"Servidor corriendo correctamente en url: localhost:3700"
+			);
+		});
+	})
+	.catch((err) => console.log(err));
---
+++ Práctica NodeJS/Sección 62.Empezando a desarrollar un backend con NodeJS/models/project.js
@@ -0,0 +1,12 @@
+"use strict";
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var projectSchema = Schema({
+	name: String,
+	description: String,
+	category: String,
+	year: Number,
+	langs: String,
+	image: String,
+});
+module.exports = mongoose.model("Project", projectSchema);
