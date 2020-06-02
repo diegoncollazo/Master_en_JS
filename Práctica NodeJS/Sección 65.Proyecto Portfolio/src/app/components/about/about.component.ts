2d3fcd437180144e871934aff2e202b0561e8a20
diegoncollazo <diego.nicolas@outlook.com.ar>
2020-06-02 02:52:25 -0300

    JS.Sección 67

--- Práctica NodeJS/Sección 65.Proyecto Portfolio/src/app/components/about/about.component.ts
+++ Práctica NodeJS/Sección 65.Proyecto Portfolio/src/app/components/about/about.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+    public title: string;
+    public subtitle: string;
+    public web: string;
+    constructor() {
+        this.title = 'Diego Collazo';
+        this.subtitle = 'Desarrolador WEB & Otros';
+        this.web = 'diego.nicolas@outlook.com.ar';
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
